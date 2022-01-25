9f15f4611b9fa6b2df3cb3ca673c77c3dec8cb36
Remco Westerhoud <remco@westerhoud.nl>
2022-01-25 14:23:02 +0100

    Initial commit

---
+++ src/main/java/com/example/techtalk/TechtalkApplication.java
@@ -0,0 +1,27 @@
+package com.example.techtalk;
+
+import io.camunda.zeebe.client.api.response.ActivatedJob;
+import io.camunda.zeebe.client.api.worker.JobClient;
+import io.camunda.zeebe.spring.client.EnableZeebeClient;
+import io.camunda.zeebe.spring.client.annotation.ZeebeDeployment;
+import io.camunda.zeebe.spring.client.annotation.ZeebeWorker;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableZeebeClient
+@ZeebeDeployment(resources = "classpath*:*.cloud-bpmn")
+public class TechtalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechtalkApplication.class, args);
+	}
+
+	@ZeebeWorker(type = "task1")
+	public void handleTask1(final JobClient jobClient, final ActivatedJob activatedJob) {
+		System.out.println("Completing task ...");
+		jobClient.newCompleteCommand(activatedJob.getKey())
+				.send()
+				.exceptionally( throwable -> { throw new RuntimeException("I broke"); });
+	}
+}
